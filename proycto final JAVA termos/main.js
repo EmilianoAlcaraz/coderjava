d1dd2fc20a5e443a26c41a855d851fa3936c6d2f
EmilianoAlcaraz <118304348+EmilianoAlcaraz@users.noreply.github.com>
2023-04-27 23:36:45 -0300

    Add files via upload

---
+++ proycto final JAVA termos/main.js
@@ -0,0 +1,33 @@
+
+
+
+let respuesta = prompt("Bienvenido a Importados Norte, a realizado compras en esta pagina?")
+if(respuesta === "si") 
+alert("Perfecto, avisanos cualquier duda!!")
+
+else{
+    if(respuesta === "no")
+    alert("Genial, eres nuevo aqui, ojala disfrutes los produtos!!")
+}
+
+
+
+let usuario = prompt("Ingrese su usuario");
+while( usuario != "Emiliano"){
+alert("Usuario ingresado incorrecto");
+usuario = prompt("ingrese nuevamente el usuario correcto");
+}
+
+
+
+
+//function saludar (saludo, nombre)
+//let mensaje = `${hola} ${emiliano}`;
+//console.log(mensaje);
+
+//saludar(hola, bienvenido)
+
+
+
+
+
