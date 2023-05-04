06588c72d5fe27b6b54158a2c0f9df548b5cbea5
EmilianoAlcaraz <118304348+EmilianoAlcaraz@users.noreply.github.com>
2023-05-04 18:22:34 -0300

    Add files via upload

--- proycto final JAVA termos/main.js
+++ proycto final JAVA termos/main.js
@@ -1,6 +1,14 @@
 
 
 
+let usuario = prompt("Ingrese su usuario");
+while( usuario != "Emiliano"){
+alert("Usuario ingresado incorrecto");
+usuario = prompt("ingrese nuevamente el usuario correcto");
+}
+
+
+alert("Bienvenido, Emiliano")
 let respuesta = prompt("Bienvenido a Importados Norte, a realizado compras en esta pagina?")
 if(respuesta === "si") 
 alert("Perfecto, avisanos cualquier duda!!")
@@ -12,20 +20,26 @@ else{
 
 
 
-let usuario = prompt("Ingrese su usuario");
-while( usuario != "Emiliano"){
-alert("Usuario ingresado incorrecto");
-usuario = prompt("ingrese nuevamente el usuario correcto");
+function saludar (saludo, nombre){
+  let mensaje = `${hola} ${emiliano}`;
+     console.log(mensaje);
 }
-alert("Bienvenido, Emiliano")
+     saludar(hola, bienvenido)
+
+
+const loginForm = document.querySelector('form');
+const loginButton = document.querySelector('#login-button');
+
+loginButton.addEventListener('click', (event) => {
+  event.preventDefault();    // Prevenir que el formulario se envíe automáticamente
+
+  const username = document.querySelector('#username').value;
+  const password = document.querySelector('#password').value;
+
+});
 
 
 
-//function saludar (saludo, nombre)
-//let mensaje = `${hola} ${emiliano}`;
-//console.log(mensaje);
-
-//saludar(hola, bienvenido)
 
 
 
