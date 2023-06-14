3ed607ac2e2cf5254f519c3a45111e4a113d1287
EmilianoAlcaraz <118304348+EmilianoAlcaraz@users.noreply.github.com>
2023-06-14 16:21:35 -0300

    Add files via upload

--- proycto final JAVA termos/contacto.js
+++ proycto final JAVA termos/contacto.js
@@ -1,11 +1,13 @@
 //pagina contacto
 
-    let usuario = prompt("Ingrese su usuario");
-    while( usuario != "Emiliano"){
-    alert("Usuario ingresado incorrecto");
-    usuario = prompt("ingrese nuevamente el usuario correcto");
-    }
-     alert("Bienvenido, Emiliano")
+function iniciarSesion() {
+  var username = document.getElementById("usernameInput").value;
+  var password = document.getElementById("passwordInput").value;
+  
+  // Aquí puedes realizar la lógica de autenticación con los datos ingresados
+  // por ejemplo, puedes enviar los datos a un servidor para verificar la autenticidad
+}
+
 
     const username = document.querySelector('#username').value;
     const password = document.querySelector('#password').value;
@@ -28,12 +30,48 @@ const loginForm = document.querySelector('form');
 const loginButton = document.querySelector('#login-button');
 
 loginButton.addEventListener('submit', (event) => {
-  event.preventDefault(); 
+  event.preventDefault();
 
-  const username = document.querySelector('#username').value;
-  const password = document.querySelector('#password').value;
+  const usernameInput = document.querySelector('#username');
+  const passwordInput = document.querySelector('#password');
 
+  const username = usernameInput.value;
+  const password = passwordInput.value;
+
+  // Aquí puedes realizar la lógica de autenticación con los datos ingresados
+  // por ejemplo, puedes verificar los datos en una base de datos simulada
+
+  // Supongamos que tenemos una base de datos simulada llamada "usersDB" en el almacenamiento local
+  let usersDB = localStorage.getItem('usersDB');
+  if (!usersDB) {
+    // Si no hay datos en la base de datos simulada, creamos un objeto vacío
+    usersDB = {};
+  } else {
+    // Si hay datos en la base de datos simulada, lo convertimos a objeto utilizando JSON.parse
+    usersDB = JSON.parse(usersDB);
+  }
+
+  // Verificar si el usuario existe en la base de datos simulada
+  if (usersDB[username]) {
+    // Verificar si la contraseña es correcta
+    if (usersDB[username] === password) {
+      // La autenticación es exitosa
+      alert('Inicio de sesión exitoso');
+      // Redirigir a otra página, mostrar contenido protegido, etc.
+    } else {
+      // La contraseña es incorrecta
+      alert('Contraseña incorrecta');
+    }
+  } else {
+    // El usuario no existe en la base de datos simulada
+    alert('Usuario no encontrado');
+  }
+
+  // Limpiar los campos de entrada después de procesar los datos
+  usernameInput.value = '';
+  passwordInput.value = '';
 });
+
 
 // funcion en orden 
 function ordenar (lista) {
@@ -43,18 +81,6 @@ function ordenar (lista) {
     const lista = ["termo stanley rosa", "termo stanley rojo", "termo stanley verde", "termo stanley azul"];
     const lista2 = ordenar(lista);
     console.log(lista2);
-
-
-
-
-
-    
-
-
-
-
-
-
 
 
 
--- proycto final JAVA termos/modelos.js
+++ proycto final JAVA termos/modelos.js
@@ -2,7 +2,7 @@
 
 const traerStock = async () => {
   try{
-    const response = await fetch ("stock.json");
+    const response = await fetch ("./stock.json");
     const stock = await response.json();
     return stock ;
 
@@ -17,30 +17,58 @@ const termosContainer = document.getElementById('termos_container');
 //** Creacion de los productos en el dom de shop segun el stock que haya *
 
 stock.then((stock) => {
-  
   stock.forEach(item => {
-      const div = document.createElement('div');
-      div.classList.add('producto');
-      div.innerHTML=`
+    const div = document.createElement('div');
+    div.classList.add('producto');
+    div.innerHTML = `
       <h3 class="producto_marca">${item.marca}</h3>
-      <h3 class="producto_color" ${item.color}</h3>
+      <h3 class="producto_color">${item.color}</h3>
       <p class="producto_precio">$${item.precio}</p>
       <button type="button" class="producto_btn">Añadir al carrito</button>
-      `
-      termosContainer.appendChild(div);
+    `;
+    termosContainer.appendChild(div);
   });
 
   const botonAgregar = document.querySelectorAll(".producto_btn");
-  
   const ArraydeBontones = Array.from(botonAgregar);
 
-  ArraydeBontones.forEach(btn =>{
-    btn.addEventListener('click', ()=>{
-      alert('Producto agregado al carrito correctamente');
-    })
-  })
-  
-})
+  ArraydeBontones.forEach(btn => {
+    btn.addEventListener('click', () => {
+      const producto = btn.parentElement;
+      const marca = producto.querySelector('.producto_marca').textContent;
+      const color = producto.querySelector('.producto_color').textContent;
+      const precio = producto.querySelector('.producto_precio').textContent;
+
+      // Crear un objeto para representar el producto agregado al carrito
+      const productoAgregado = {
+        marca: marca,
+        color: color,
+        precio: precio
+      };
+
+      // Obtener los productos del carrito del almacenamiento local
+      let productosCarrito = localStorage.getItem('productosCarrito');
+      if (productosCarrito) {
+        // Si ya hay productos en el carrito, convertirlos a un array
+        productosCarrito = JSON.parse(productosCarrito);
+      } else {
+        // Si no hay productos en el carrito, crear un array vacío
+        productosCarrito = [];
+      }
+
+      // Agregar el producto actual al array de productos del carrito
+      productosCarrito.push(productoAgregado);
+
+      // Guardar los productos del carrito actualizados en el almacenamiento local
+      localStorage.setItem('productosCarrito', JSON.stringify(productosCarrito));
+
+      // Mostrar el nombre del producto agregado en la alerta
+      alert('Producto ' + marca + ' agregado al carrito correctamente');
+    });
+  });
+});
+
+
 
 
 
